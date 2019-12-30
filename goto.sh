ab707c6719b1c277eb0161931a4e5096eabde8aa
Eduardo A.F <eacevedof@yahoo.es>
2019-12-30 16:31:39 +0100

    goto.sh ok

--- goto.sh
+++ goto.sh
@@ -1,12 +1,26 @@
 #!/bin/sh
+
+# array de proyectos
 declare -a projects=(
-    "prj_docker" "prj_docker_ci" "prj_docker_imgs" "prj_doctrine2" "prj_elchalanaruba" 
-    "prj_flutter" "prj_js" "prj_jsonup" "prj_jswebpack" "prj_linux" "prj_mysqlhive" "prj_phptests" 
-    "prj_platziphp" "prj_python37" "prj_reactjs3" "prj_symfony" "prj_theframework" 
-    "prj_theframework_helpers" "prj_wordpress"
+      "prj_docker" "prj_docker_ci" "prj_docker_imgs" "prj_doctrine2" "prj_elchalanaruba" 
+      "prj_flutter" "prj_js" "prj_jsonup" "prj_jswebpack" "prj_linux" "prj_mysqlhive" "prj_phptests" 
+      "prj_platziphp" "prj_python37" "prj_reactjs3" "prj_symfony" "prj_theframework" 
+      "prj_theframework_helpers" "prj_wordpress" "prj_bash"
     )
 
-for i in "${projects[@]}"
-do
-    echo $i
-done
+#echo $1
+input=$1
+project="prj_$input"
+#echo $project
+#exit
+
+if [[ " ${projects[@]} " =~ " ${project} " ]]; then
+  #echo "executing cd"
+  fullpath="/e/projects/$project"
+  # echo $fullpath
+  cd $fullpath
+  exec bash
+  # cd "$(dirname "${fullpath}")"
+else
+  echo "project not found in array"
+fi
