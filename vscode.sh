40df1fe9a8622dc9e700069a0065345aba3a8a0e
Eduardo A.F <eacevedof@yahoo.es>
2019-12-30 17:32:37 +0100

    mejoro vscdoe.sh ahora abre pwd si no hay argumentos

--- vscode.sh
+++ vscode.sh
@@ -6,13 +6,19 @@
 
 #capturo el primer argumento
 arg1=$1
+
 #formo el nombre del proyecto
 prjfolder="prj_$arg1"
-
 fullpath="/e/projects/$prjfolder"
+# si no hay argumento (nombre del proyecto)
+if [[ -z "$arg1" ]]; then
+  # abro donde estoy
+  fullpath=$PWD
+fi
+
 if [[ ! -d $fullpath ]]; then 
-    echo "not dir: $fullpath"
-    exit 1
+  echo "not dir: $fullpath"
+  exit 1
 fi
 
 code $fullpath
