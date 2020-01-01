e2473115936378088ccaaf7c6115ee373ef7f7ed
Eduardo A.F <eacevedof@yahoo.es>
2020-01-01 16:16:33 +0100

    backup udemy.py

--- py/routines/udemy.py
+++ py/routines/udemy.py
@@ -2,10 +2,23 @@
 import sys
 from pprint import pprint
 
+def printx(mxvar):
+
+    if isinstance(mxvar,list):
+        for i, item in enumerate(mxvar):
+            print(i," => ",item)
+    else:
+        print(mxvar)
+
+def pr(var,title=""):
+    if title!="":
+        print(title)
+    printx(var)
+
 def pd(var,title=""):
     if title!="":
         print(title)
-    print(var)
+    printx(var)
     sys.exit()
 
 def file_get_contents(filename):
@@ -15,30 +28,30 @@ def file_get_contents(filename):
     except IOError:
         return f"no file found: {filename}"
 
-def get_numberpos(arlines):
-    arnumlines = []
-    for ipos, strline in enumerate(arlines):
-        if strline[0].isdigit() and strline.find(" min")!=-1:
-            arnumlines.append(ipos)
-    return arnumlines
-
-
 def get_titles_pos(arclean):
     arpostitl = []
     for ipos, strline in enumerate(arclean):
         # si empieza por numero y no tiene la palabra min
-        if strline[0].isdigit() and strline.find(" min")!=-1:
+        if strline[0].isdigit() and strline.find(" min")==-1:
             arpostitl.append(ipos)
     return arpostitl
 
 def get_title_w_min(ipostitle,arpostitl,arclean):
-    idxtitl = arpostitl.index(ipostitle)
-    pd(idxtitl,"idxtitle")
-    iend = arpostitl[idxtitl+1]
+    pr(arpostitl,"getwmin: arpostitl")
+    pr(arclean,"getwmin: arclean")
+    pr(ipostitle,"getwmin: ipostitle")
+    idx = arpostitl.index(ipostitle)
+
+    
+    # pr(idx,"idx")
+    iend = arpostitl[idx+1]
+    pr(iend,"iend")
     arconcat = []
-    for strline in arclean[ipostitle:iend]:
-        arconcat.append(strline)
-    strtitle = " ".join[arconcat]
+    for i, strline in enumerate(arclean):
+        if(i>=ipostitle and i<=iend):
+            arconcat.append(strline)
+    #pd(arconcat,"arconcat")
+    strtitle = " ".join(arconcat)
     return strtitle
 
 def index(pathfile):
@@ -50,9 +63,9 @@ def index(pathfile):
     arpostitl = get_titles_pos(arclean)
     # pprint(arpostitl)
     for i, ipostitle in enumerate(arpostitl):
-        print(f"{i} {ipostitle}")
+        # print(f"{i} {ipostitle}")
         strtitle = get_title_w_min(ipostitle, arpostitl, arclean)
-        
+        pr(strtitle,"strtitle")
 
 
 
