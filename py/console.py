e26cb42df6551993489f04536b9b99b9891aefd8
Eduardo A.F <eacevedof@yahoo.es>
2020-01-01 14:09:45 +0100

    configuro validacion de argumentos

--- py/console.py
+++ py/console.py
@@ -4,8 +4,22 @@ import sys
 from pprint import pprint
 from routines.udemy import *
 
-print('Number of arguments:', len(sys.argv), 'arguments.')
-print('Argument List:', str(sys.argv))
+# print('Number of arguments:', len(sys.argv), 'arguments.')
+# print('Argument List:', str(sys.argv))
 
+def getarg(ipos=0):
+    return sys.argv[ipos]
 
-index()
+argument = getarg(1)
+module = getarg(2)
+funcname = getarg(3)
+
+if module=="udemy":
+    if funcname=="index":
+        index()
+    else:
+        pprint("no func found")
+        pprint(funcname)
+else:
+    pprint("no module found")
+    print(module)
