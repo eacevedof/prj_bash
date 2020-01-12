7a80465ccde8049a852ed6b0220330c0b9fb0d83
ioedumac <eacevedof@yahoo.es>
2020-01-12 14:48:06 +0100

    parece que funciona utils.sh

--- dockers.sh
+++ dockers.sh
@@ -1,8 +1,19 @@
 #!/bin/sh
 
 # dockers.sh <cont-NAMES>|<CONTAINER ID> <open new win bash>
+paththis=$PWD
+
 containerid=$1
 iswinbash=$2
+
+
+# compruebo si existe el comando de windows start
+if [ -x "$(command -v start)" ]; then
+  # nueva ventana bash
+  start E:\\programas\\x64\\git\\git-bash.exe --cd="$fullpath"
+else
+  open -a Terminal $fullpath
+fi
 
 if [[ ! -z "$iswinbash" ]]; then
   # start E:\\programas\\x64\\git\\git-bash.exe --cd="$PWD" --login -i -c "docker ps"
---
+++ utils.sh
@@ -0,0 +1,75 @@
+#!/bin/sh
+
+# referencia https://linuxhint.com/return-string-bash-functions/
+#file: utils.sh
+
+get_so () 
+{
+  case "$(uname -s)" in
+    Darwin)
+      echo "ios"
+      # return "ios"
+      ;;
+
+    Linux)
+      echo "linux"
+      # return "linux"
+      ;;
+
+    CYGWIN*|MINGW32*|MSYS*)
+      echo "windows"
+      # return "windows"
+      ;;
+    # Add here more strings to compare
+    # See correspondence table at the bottom of this answer
+    *)
+      # return "unknown"
+      echo "unknown"
+      ;;
+  esac
+}
+
+is_true ()
+{
+  if [ -z "$2" ]; then return 0; fi
+  if [ -z "$1" ]; then return 0; fi
+
+  # echo "is_true: 2: $2, 1:$1"
+  if [ "$1" == "$2" ]; then return 0;  else return 1; fi
+}
+
+is_truestr()
+{
+  if is_true $1 $2; then echo "0"; else echo "1"; fi
+}
+
+is_win ()
+{
+  local strso=$(get_so)
+  # echo "strso: $strso"
+  # if [[ $strso == "windows" ]]; then echo "0";  else echo "1"; fi
+  is_truestr $strso "windows"
+  
+}
+
+is_ios ()
+{
+  local strso=$(get_so)
+  # echo "strso: $strso"
+  if [[ $strso == "ios" ]]; then echo "0";  else echo "1"; fi
+}
+
+# is_true "xx" "yy"
+# echo ${ is_true "aa" "jj" }
+
+#value = $((is_true "xx" "xx"))
+
+#echo $value
+# get_so
+#value=$(is_win)
+# echo $value
+
+# is_truestr "xx" "yy"
+
+# is_win
+# is_ios
