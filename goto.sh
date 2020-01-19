9bc93a081d9a5bc2eadad1a4a84041ebd36cf1e8
ioedumac <eacevedof@yahoo.es>
2020-01-19 15:08:29 +0100

    mejoro goto.sh

--- goto.sh
+++ goto.sh
@@ -1,16 +1,23 @@
 #!/bin/sh
 
-#capturo el primer argumento
-arg1=$1
+# import utils.sh
+thisdir=$(dirname "$0")
+. "$thisdir/utils.sh"
+
 #formo el nombre del proyecto
-prjfolder="prj_$arg1"
+prjfolder="prj_$1"
 fullpath="/e/projects/$prjfolder"
+if is_ios; then fullpath=$PATHPRJ"/$prjfolder"; fi
 
 # si es directorio
 if [[ -d $fullpath ]]; then
   # cambia de directorio en la misma ventana
   cd $fullpath
-  exec bash
+  if is_win; then exec bash; fi
+  if is_ios; then 
+    # open -a Terminal $fullpath; abre pero no en ruta
+    osascript -e "tell application \"Terminal\" to do script \"cd '$fullpath'\""
+  fi
 else
   echo "folder: $fullpath not found"
 fi
