261ab110708aefea0ce60ac98dbffa6e092087bf
ioedumac <eacevedof@yahoo.es>
2020-05-06 23:43:19 +0200

    react.py

--- py/routines/react.py
+++ py/routines/react.py
@@ -1,4 +1,5 @@
 # routines.react.py
+print("routines.react.py")
 from tools.tools import file_get_contents,pr,pd,file_put_contents
 
 # obtengo el contenido el archivo de configuracion .pysh
@@ -8,11 +9,27 @@ def get_pysh_lines(pathfile):
     return arlines
 
 def get_pysh_dict(arlines):
-    pass
+    configdic = []
+    for line in arlines:
+        artemp = line.split("=")
+        print(artemp[0])
+        print(artemp[1])
+        configdic.append({artemp[0]:artemp[1]})
+    return configdic
+
+def get_config(pathfile):
+    arlines = get_pysh_lines(pathfile)
+    diconfig = get_pysh_dict(arlines)
+    return diconfig
 
 def index(pathfile):
-    print(pathfile)
-    print("routines.react.py")
+    config = get_config(pathfile)
+
+    print(config)
+ 
+
+
+    
     # obtener la configuracion de .pysh
     # copiar contenido de PATH_BUILD a PATH_PUBLIC
         # no copiar el index.html
