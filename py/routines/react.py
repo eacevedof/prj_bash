0ac77205f4be1dc5801d3682c02e2ec2ebdbb42d
ioedumac <eacevedof@yahoo.es>
2020-05-07 00:44:02 +0200

    react.py end

--- py/routines/react.py
+++ py/routines/react.py
@@ -1,6 +1,9 @@
 # routines.react.py
+# py.sh "/Users/ioedu/projects/prj_doblerr/frontend_react/pannel/.pysh" index react
 print("routines.react.py")
 from tools.tools import file_get_contents,pr,pd,file_put_contents
+import shutil
+import os
 
 # obtengo el contenido el archivo de configuracion .pysh
 def get_pysh_lines(pathfile):
@@ -12,8 +15,8 @@ def get_pysh_dict(arlines):
     configdic = []
     for line in arlines:
         artemp = line.split("=")
-        print(artemp[0])
-        print(artemp[1])
+        # print(artemp[0])
+        # print(artemp[1])
         configdic.append({artemp[0]:artemp[1]})
     return configdic
 
@@ -22,14 +25,32 @@ def get_config(pathfile):
     diconfig = get_pysh_dict(arlines)
     return diconfig
 
+def get_key(config,key):
+    for cfg in config:
+        if key in cfg:
+            return cfg[key]
+    return ""
+
 def index(pathfile):
     config = get_config(pathfile)
+    # print(config)
+    pathbuild = get_key(config,"PATH_BUILD")
+    pathpublic = get_key(config,"PATH_PUBLIC")
+    pathtwig = get_key(config,"PATH_INDEX_TWIG")
 
-    print(config)
- 
+    print("...removing pathpublic: "+pathpublic)
+    shutil.rmtree(pathpublic)
+    print("...copying pathbuild: "+pathbuild)
+    shutil.copytree(pathbuild, pathpublic)
+    print("...removing index.html")
+    os.remove(pathpublic+"/index.html")
+    print("...changing name of asset-manifest.json")
+    os.rename(pathpublic+"/asset-manifest.json", pathpublic+"/manifest.json")
+    print("...copying index.html to twig: "+pathtwig)
+    shutil.copyfile(pathbuild+"/index.html", pathtwig)
+    print("process end")
 
 
-    
     # obtener la configuracion de .pysh
     # copiar contenido de PATH_BUILD a PATH_PUBLIC
         # no copiar el index.html
