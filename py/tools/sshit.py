9b51187a1b0c1b00fd805b397e1b4dd91edcf2a9
ioedumac <eacevedof@yahoo.es>
2020-05-23 18:21:18 +0200

    deploy: sshit con spur

--- py/tools/sshit.py
+++ py/tools/sshit.py
@@ -1,11 +1,8 @@
-import os
-import sys
-from pexpect import pxssh
-import getpass from getpass
+import spur
 
 class Sshit:
 
-    objserv = None
+    shell = None
     dicaccess = None
 
     def __init__(self, dicaccess):
@@ -17,24 +14,23 @@ class Sshit:
             print(f"Sshit: no acces data supplied")
             return 0
 
-        s = pxssh.pxssh()
         host = self.dicaccess["host"],
-        is_logged = s.login(self.dicaccess["host"],self.dicaccess["username"],self.dicaccess["password"]):
-        if not is_logged:
+        shell = spur.SshShell(self.dicaccess["host"],self.dicaccess["username"],self.dicaccess["password"]):
+        if not shell:
             print(f"Sshit: not connected to host: {host}")
             return 0
-        self.objserv = s
+        self.shell = shell
         print(f"Sshit: connected to host: {host}")
 
     def command(self,strcmd):
-        if self.objserv is None:
+        if self.shell is None:
             print(f"Sshit: command {strcmd} not excecuted. Not connected to host")
             return 0
-        objserv = self.objserv
-        child = objserv.spawn(strcmd,encoding="utf-8")
-        child.logfile = sys.stdout
-        # objserv.sendline(strcmd)
+        shell = self.shell
+        with shell:
+            result = shell.run(strcmd)
+        print(result.output)
 
     def close(self):
-        if self.objserv is not None:
-            self.objserv.logout()
+        if self.shell is not None:
+            pass
