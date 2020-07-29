1ceb4f218cf462e4d28f47ba4419b9ff3dd402a2
ioedumac <eacevedof@yahoo.es>
2020-07-29 12:36:12 +0200

    reset ide

---
+++ py/routines/resetide.py
@@ -0,0 +1,6 @@
+# https://tsukie.com/en/technologies/how-to-reset-trial-time-for-jetbrains-products/
+#1. Terminate your JetBrains application
+#2. Remove all *.key files in ~/Library/Preferences/<Your Jetbrains product name>/eval/ folder
+#3. remove ~/Library/Preferences/<Your Jetbrains product name>/options/options.xml
+#4. Remove any Jetbrains related keys in
+#5. Remove all JetBrains related plist files in ~/Library/Preferences/
