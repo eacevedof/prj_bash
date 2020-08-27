caa3c22fa111459b43524b8b87eac34f6595895d
ioedumac <eacevedof@yahoo.es>
2020-08-27 17:24:50 +0200

    ya hace login

--- py/routines/reactgen.py
+++ py/routines/reactgen.py
@@ -2,19 +2,21 @@
 # py.sh reactgen <dbname> <table>
 
 from tools.tools import scandir, get_datetime, pr, copydir
+import json
 import shutil
 import os
 import requests
 
 def get_token(urllogin):
-    r = requests.get(urllogin, data={"user":"fulanito","password":"menganitox"}, headers={"Origin":"http://localhost:3000"})
-    return r
+    r = requests.post(urllogin, data={"user":"fulanito","password":"menganitox"}, headers={"Origin":"http://localhost:3000"})
+    dic = json.loads(r.text)
+    return dic["data"]["token"]
 
 # conectar con la bd
 def get_tpl():
     tpl = {
-        "endpoint-login": "http://localhost:10000/apifiy/security/login"
-        "endpoint-fields": "http://localhost:10000/apify/fields/c4/db-tinymarket/%table%"
+        "endpoint-login": "http://localhost:10000/apifiy/security/login",
+        "endpoint-fields": "http://localhost:10000/apify/fields/c4/db-tinymarket/%table%",
         "pathmodule" : "/Users/ioedu/projects/prj_tinymarket_front/frontend_react/restrict/src/modules/product",
         "entity": "product",
         "entity-singular": "Product",
@@ -43,7 +45,7 @@ def index(table):
     pr(token,"token")
 
     paththemp = dictpl["pathtemp"]
-    copydir(dictpl["pathmodule"], paththemp)
+    #copydir(dictpl["pathmodule"], paththemp)
     #pr(dictpl)
     files = scandir(paththemp)
     pr(files,f"files en {pathtemp}")
