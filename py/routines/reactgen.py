088c45fc2756a25f6fe1d258b0f1c7835a258bf5
ioedumac <eacevedof@yahoo.es>
2020-08-27 15:45:57 +0200

    reactgen.py configurando tpl

--- py/routines/reactgen.py
+++ py/routines/reactgen.py
@@ -1,4 +1,4 @@
-# routines.image.py
+# routines.reactgen.py
 # py.sh reactgen <dbname> <table>
 
 from tools.tools import is_dir, scandir, get_now, pr, mkdir, get_basename
@@ -6,8 +6,17 @@ import shutil
 import os
 
 # conectar con la bd
+def get_tpl():
+    tpl = {
+        "pathmodule":"/Users/ioedu/projects/prj_tinymarket_front/frontend_react/restrict/src/modules/product"
+    }
+    return tpl
+
 
 def index(db, table):
     pr(db,"db")
     pr(table,"table")
-
+    # módulo plantilla
+    dictpl = get_tpl()
+    pr(dictpl)
+    
