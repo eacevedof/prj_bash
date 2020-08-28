6526ad496f7738c085e1f9f8772e7fff13ea5843
ioedumac <eacevedof@yahoo.es>
2020-08-28 12:21:07 +0200

    phpstorm

---
+++ py/routines/phpstorm.py
@@ -0,0 +1,27 @@
+from tools.tools import pr
+
+dicconf = {
+    "pathcache": "/Users/ioedu/Library/Caches/", # PhpStorm2020 # ls -lat | grep Jet
+
+    "pathphp": "/Users/ioedu/Library/Application Support/PhpStorm",  #webview
+
+    #"pathjet1": "/Users/ioedu/Library/Application Support/JetBrains", #evaluation key
+    "pathjet2": "/Users/ioedu/Library/Application Support/JetBrains/consentOptions", #flag accepted
+    "pathjet3": "/Users/ioedu/Library/Application Support/JetBrains/PhpStorm2020.1/eval", #eval key
+
+
+    # https://trello-attachments.s3.amazonaws.com/5ecce8fe2983ed33bd68451c/1056x466/c9ffe4b4c7d1fc5c7d1131ac93525e98/image.png
+    "pathplist1": "/Users/ioedu/Library/Preferences/jetbrains.phpstorm.aba76028.plist",
+    
+    #https://trello-attachments.s3.amazonaws.com/5ecb901159c8b07cc0acc96c/5ecce8fe2983ed33bd68451c/1503497d7d710eab81343e7fb9d74ab7/image.png
+    "pathplist2": "/Users/ioedu/Library/Preferences/com.jetbrains.PhpStorm.plist",
+
+    #https://trello-attachments.s3.amazonaws.com/5ecb901159c8b07cc0acc96c/5ecce8fe2983ed33bd68451c/a72f26e83e6612c57f5bdee563753b92/image.png
+    "pathplist3": "/Users/ioedu/Library/Preferences/jetbrains.jetprofile.asset.plist",
+
+    # https://trello-attachments.s3.amazonaws.com/5ecb901159c8b07cc0acc96c/5ecce8fe2983ed33bd68451c/5f69b438df665206ee82b29a0154be2d/image.png
+    # "pathxmlmanual": "/Users/ioedu/Library/Preferences/com.apple.java.util.prefs.plist", #manual quitar entradas jetbrains 
+}
+
+def index():
+    pr("hola")
--- py/routines/reactgen.py
+++ py/routines/reactgen.py
@@ -1,7 +1,7 @@
 # routines.reactgen.py
 # py.sh reactgen <dbname> <table>
 
-from tools.tools import scandir, get_datetime, pr, copydir
+from tools.tools import scandir, get_datetime, pr, copydir, file_get_contents, file_put_contents
 import json
 import shutil
 import os
@@ -32,6 +32,7 @@ def get_tpl():
         "endpoint-login": "http://localhost:10000/apifiy/security/login",
         "endpoint-fields": "http://localhost:10000/apify/fields/c4/db-tinymarket/%table%",
         "pathmodule" : "/Users/ioedu/projects/prj_tinymarket_front/frontend_react/restrict/src/modules/product",
+        "table":"app_product"
         "entity": "product",
         "entity-singular": "Product",
         "entity-plural": "Products",
@@ -43,11 +44,42 @@ def get_tpl():
 
 def get_dest():
     tpl = {
-        "entity": "product",
-        "entity-singular": "Product",
-        "entity-plural": "Prooducts",
+        "entity": "apparray",
+        "entity-singular": "App Array",
+        "entity-plural": "App Arrays",
     }
     return tpl
+
+def get_jsfiles(pathfolder):
+    files = scandir(pathfolder)
+    files = [file for file in files if file.find(".js")!=-1]
+    return files
+
+
+def replace_in_file(pathfile, dicfr={}):
+    if not bool(dicfr):
+        return 
+
+    content = file_get_contents(pathfile)
+    for k in dicfr:
+        content = content.replace(k, dicfr[k])
+    file_put_contents(pathfile, content)
+
+
+
+def replace_queries(pathfolder, dicfrom, dicto):
+    if not is_dir(pathfolder)
+        return 
+
+    files = get_jsfiles(pathfolder)
+    dicmerge = {}
+    for k in dicfrom:
+        if dicto.get(k):
+            dicmerge[dicfrom[k]] = dicto[k]
+
+    for file in files:
+        pathfile = pathfolder.c
+    
 
 
 def index(table):
