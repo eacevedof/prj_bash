72d120cdc9b141d8771097fb531678393376d11c
ioedumac <eacevedof@yahoo.es>
2021-05-21 19:00:23 +0200

    obtengo ya el eschema

--- py/routines/reactmodel.py
+++ py/routines/reactmodel.py
@@ -5,27 +5,35 @@ import requests
 
 BASE_URL = "http://localhost:900"
 USER = "fulanito"
-PASSWORD = "MaFaLDa1234ABC"
+PASSWORD = "MaFaLDa1234"
 
 
 def login() -> str:
     url = f"{BASE_URL}/apify/security/login"
-    data = {"user":USER,"password":PASSWORD}
+    post = {"user":USER,"password":PASSWORD}
     req = requests.Session()
     req.headers.update({"origin":"http://localhost:3000"})
-    r = req.post(url, data=data)
+    r = req.post(url, data=post)
     dict = r.json()
     return dict.get("data",{}).get("token","")
 
-def get_metadada(tablename):
+def get_metadada(tablename, token) -> dict:
+    url = f"{BASE_URL}/apify/fields/c1/db-eafpos/{tablename}"
+    post = {"apify-usertoken":token}
 
-    pass
+    req = requests.Session()
+    req.headers.update({"origin":"http://localhost:3000"})
+    r = req.post(url, data=post)
+    dict = r.json()
+    return dict.get("data",[])
+
 
 def index(tablename):
     pr(tablename)
     token = login()
     if token:
-        pr(token)
+        metadata = get_metadada(tablename, token)
 
+        pr(metadata)
 
 
