353d8b8574d5bf7f76458922b9a57604a50b8ae4
ioedumac <eacevedof@yahoo.es>
2021-05-22 22:13:58 +0200

    react_crud replace ok

--- py/tools/helpers/react_crud.py
+++ py/tools/helpers/react_crud.py
@@ -2,12 +2,12 @@ from py.tools.tools import mkdir, scandir, get_datetime, pr, file_get_contents, 
 
 PATH_MODULE = "/Users/ioedu/projects/prj_eafpos/frontend/restrict/src/modules"
 FOLDER_TEMPLATE = "zzz-tpl"
-SYS_FIELDS = [
 
-]
-replaces = {
+REPLACES = {
     "zzz-tpls": "app-tables",
     "zzz-tpl": "app-table",
+    "zzz_tpls": "app_tables",
+    "zzz_tpl": "app_table",
     "ZzzTpls": "AppTables",
     "ZzzTpl": "AppTable",
     "Tpls": "Tables",
@@ -30,7 +30,12 @@ class ReactCrud:
         self.__mod_folder = path
         mkdir(path)
 
-    def __sync_folder(self):
+    def __save_replaced(self, path_from: str, path_to: str):
+        content = file_get_contents(path_from)
+        content = self.__get_replaced(content)
+        file_put_contents(path_to, content)
+
+    def __async_folder(self):
         path_from = f"{PATH_MODULE}/{FOLDER_TEMPLATE}/async"
         path_to = f"{PATH_MODULE}/{self.__tmp_folder}/async"
 
@@ -40,21 +45,41 @@ class ReactCrud:
         for strfile in files:
             if ".js" not in strfile:
                 continue
+            self.__save_replaced(f"{path_from}/{strfile}", f"{path_to}/{strfile}")
 
-            path_file_from = f"{path_from}/{strfile}"
-            content = file_get_contents(path_file_from)
+    def __config_folder(self):
+        path_from = f"{PATH_MODULE}/{FOLDER_TEMPLATE}/config"
+        path_to = f"{PATH_MODULE}/{self.__tmp_folder}/config"
 
-            path_file_to = f"{path_to}/{strfile}"
-            content = self.__get_replaced(content)
-            file_put_contents(path_file_to, content)
+        mkdir(path_to)
+        files = scandir(path_from)
+
+        for strfile in files:
+            if ".js" not in strfile:
+                continue
+            self.__save_replaced(f"{path_from}/{strfile}", f"{path_to}/{strfile}")
+
+    def __views_folder(self):
+        path_from = f"{PATH_MODULE}/{FOLDER_TEMPLATE}/views"
+        path_to = f"{PATH_MODULE}/{self.__tmp_folder}/views"
+
+        mkdir(path_to)
+        files = scandir(path_from)
+
+        for strfile in files:
+            if ".js" not in strfile:
+                continue
+            self.__save_replaced(f"{path_from}/{strfile}", f"{path_to}/{strfile}")
 
 
-    def __get_replaced(self, content):
-        for tag in replaces:
-            value = replaces[tag]
-            content.replace(tag, value)
+    def __get_replaced(self, content: str):
+        for tag in REPLACES:
+            value = REPLACES[tag]
+            content = content.replace(tag, value)
         return content
 
     def run(self):
         self.__create_dir()
-        self.__sync_folder()
+        self.__async_folder()
+        self.__config_folder()
+        self.__views_folder()
