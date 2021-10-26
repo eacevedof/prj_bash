67ba423bc3144cca1852fa3fc5d18e49b9127164
ioedumac <eacevedof@yahoo.es>
2021-10-26 21:26:44 +0200

    lista de proyectos ok

--- py/routines/projects.py
+++ py/routines/projects.py
@@ -5,15 +5,20 @@ def index():
     list_data = get_dicconfig().get_data()
     projects = []
     for dc in list_data:
+        keys = list(dc.keys())
+        keys = [k for k in keys if "id" not in k]
+        keys.sort()
+
         projects.append({
             "id": dc.get("id", ""),
-            "options": [k for k in dc.keys() if k!="id"].sort()
+            "options": keys
         })
-    projects.sort()
+
 
     for i, dc in enumerate(projects):
         id = dc.get("id","")
         print(f"({i}) {id}")
-        for j, opt in dc.get("options",[]):
-            print(f"  - ({j}) {opt}")
+        options = dc.get("options",[])
+        for j, opt in enumerate(options):
+            print(f"  - [{j}] {opt}")
 
