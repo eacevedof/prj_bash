939a1431ce381d5b0d86d1ae6755a5cf19968c14
ioedumac <eacevedof@yahoo.es>
2021-12-25 18:05:09 +0100

    component_mysql.py

--- py/components/component_mysql.py
+++ py/components/component_mysql.py
@@ -16,7 +16,7 @@ mycursor = mydb.cursor()
 class ComponentMysql:
 
     def __init__(self, arconn = Dict):
-        self.__iserror = False
+        self.__arerrors = []
         self.__ifoundrows = 0
         self.__iaffectedrows = 0
         self.__ilastid = -1
@@ -25,7 +25,7 @@ class ComponentMysql:
 
     def __get_connection(self):
         if not self.__connection:
-            self.__connection mysql.connector.connect(
+            self.__connection = mysql.connector.connect(
                 host=self.__arconn.get("server",{}),
                 user=self.__arconn.get("user",""),
                 password=self.__arconn.get("password",""),
@@ -36,6 +36,7 @@ class ComponentMysql:
     def close(self) -> None:
         if self.__connection and self.__connection.is_connected():
             self.__connection.close()
+        self.__connection = None
 
     def query(self, sql: str) -> List:
         try:
@@ -43,14 +44,18 @@ class ComponentMysql:
             cursor = conn.cursor(dictionary=True)
             cursor.execute(sql)
             result = cursor.fetchall()
-            self.__ifoundrows = cursor.rowcount
+            self.__ifoundrows = self.__get_found_rows()
+            self.__iaffectedrows = cursor.rowcount
             return result
-        except mysql.connector.Error as e:
-            print("Error reading data from MySQL table", e)
+        except mysql.connector.Error as error:
+            self.__arerrors.append(error)
         finally:
             cursor.close()
 
-    def execute(self, sql: str):
+    def __get_found_rows(self):
+        return self.query("SELECT FOUND_ROWS()")
+
+    def exec(self, sql: str):
         try:
             conn = self.__get_connection()
             cursor = conn.cursor()
@@ -58,6 +63,6 @@ class ComponentMysql:
             conn.commit()
             self.__iaffectedrows = cursor.rowcount
         except mysql.connector.Error as error:
-            print("Failed to insert record into Laptop table {}".format(error))
+            self.__arerrors.append(error)
         finally:
             cursor.close()
