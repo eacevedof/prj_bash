6fc1b86daf652189090be32e270feff5d3a66470
ioedumac <eacevedof@yahoo.es>
2021-12-25 19:11:25 +0100

    component_crud

--- py/components/component_crud.py
+++ py/components/component_crud.py
@@ -1,5 +1,6 @@
 class ComponentCrud:
     def __init__(self):
+        self.__table = ""
         self.__arinsertfv = []
         self.__arupdatefv = []
         self.__arpks = []
@@ -9,7 +10,17 @@ class ComponentCrud:
         self.__arorderby = []
         self.__argroupby = []
         self.__arnumeric = []
+        self.__arlimit = []
+        self.__sql = ""
+        self.__querycomment = ""
+        self.__isfoundrows = False
+        self.__isdistinct = False
 
 
+    def get_select_from(self):
+        self.__sql = "-- get_selectfrom"
+        if not self.__table or not self.__argetfields:
+            return self.__sql
+        querycomment = self.__querycomment if self.__querycomment else ""
 
 
