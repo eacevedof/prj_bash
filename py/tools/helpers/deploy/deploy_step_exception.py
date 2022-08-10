39d6a1743eabd1deb979a5d261b981862589e85e
ioedumac <eacevedof@yahoo.es>
2022-08-10 12:07:51 +0200

    ERROR WITH COLOR

--- py/tools/helpers/deploy/deploy_step_exception.py
+++ py/tools/helpers/deploy/deploy_step_exception.py
@@ -1,4 +1,18 @@
+class color:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 
 class DeployStepException(Exception):
     """Forces exception from tag"""
-    pass
+
+    def __init__(self, message):
+        self.message = f"{color.FAIL}{message}{color.ENDC}"
+        super().__init__(self.message)
