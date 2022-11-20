c607f2cfe8b1186ba169082209fb055b276114c2
ioedumac <eacevedof@yahoo.es>
2022-11-20 16:02:03 +0100

    loop poc

--- pocs/loop-1.sh
+++ pocs/loop-1.sh
@@ -1,22 +1,16 @@
-#!/usr/local/bin/bash
-
-for num in 1 2 3 4 5
-do
-	echo "number $num"
-done
+#!/bin/bash
 
 # loop todos los directorios
 pathdir=$PATHPRJ
 for file in $(ls $pathdir) 
 do
-	echo "check file $pathdir/$file"
+	echo "check if file $pathdir/$file is dir"
 	if [ ! -d "$pathdir/$file" ]; then
 		continue
 	fi
 	echo "is a dir $file"
 done
 
-
+# loop en una linea
 for file in $(ls ./*); do if [ ! -d "./$file" ]; then continue; fi done
 
-#for num in 1 2 3; do echo "number i $num"; done
