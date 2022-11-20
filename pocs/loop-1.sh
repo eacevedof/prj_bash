1da1f6d047f1e7eabb16ce3ee4076a53e43db5cb
ioedumac <eacevedof@yahoo.es>
2022-11-20 14:31:05 +0100

    poc loop-1

--- pocs/loop-1.sh
+++ pocs/loop-1.sh
@@ -3,9 +3,15 @@ do
 	echo "number $num"
 done
 
+# loop todos los directorios
 for file in $(ls ./*) 
 do
-	if [ -d "$file" ]; then
-		echo "$file is a dir"
+	if [ ! -d $file ]; then
+		continue
 	fi
+	echo "is a dir $file"
 done
+
+for file in $(ls ./*); do if [ ! -d $file ]; then continue; fi; done
+
+#for num in 1 2 3; do echo "number i $num"; done
