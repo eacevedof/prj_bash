9d79d493c2ce5d54162c138754993317f72eb820
ioedumac <eacevedof@yahoo.es>
2022-11-20 15:21:31 +0100

    poc loop-1

--- pocs/loop-1.sh
+++ pocs/loop-1.sh
@@ -1,17 +1,21 @@
+#!/usr/local/bin/bash
+
 for num in 1 2 3 4 5
 do
 	echo "number $num"
 done
 
 # loop todos los directorios
-for file in $(ls ./*) 
+pathdir="/Users/ioedu"
+for file in $(ls $pathdir) 
 do
-	if [ ! -d $file ]; then
+	echo "handling file $file"
+	if [ ! -d "$pathdir/$file" ]; then
 		continue
 	fi
 	echo "is a dir $file"
 done
 
-for file in $(ls ./*); do if [ ! -d $file ]; then continue; fi; done
+for file in $(ls ./*); do if [ ! -d "./$file" ]; then continue; fi done
 
 #for num in 1 2 3; do echo "number i $num"; done
