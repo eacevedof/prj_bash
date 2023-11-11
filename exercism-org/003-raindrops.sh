155a98f31b49667675c71ab438fff43f843f0a4f
ioedumac <eacevedof@yahoo.es>
2023-11-11 17:59:53 +0100

    raindrops.sh

--- exercism-org/003-raindrops.sh
+++ exercism-org/003-raindrops.sh
@@ -7,6 +7,7 @@
 #has 7 as a factor, add 'Plong' to the result.
 #does not have any of 3, 5, or 7 as a factor, the result should be the digits of the number.
 
+# el doble parentesis se usa para operaciones aritmeticas
 is_3_factored() {
   (( $1 % 3 == 0 )) && return 0 || return 1
 }
