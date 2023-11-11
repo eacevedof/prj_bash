9b20d89feb4430fd3b3ebff9a8b1ed05cc08a4ee
ioedumac <eacevedof@yahoo.es>
2023-11-11 17:16:34 +0100

    raindrops

---
+++ exercism-org/003-raindrops.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+#https://exercism.org/tracks/bash/exercises/raindrops/edit
+
+# The rules of raindrops are that if a given number:
+#has 3 as a factor, add 'Pling' to the result.
+#has 5 as a factor, add 'Plang' to the result.
+#has 7 as a factor, add 'Plong' to the result.
+#does not have any of 3, 5, or 7 as a factor, the result should be the digits of the number.
+
+main() {
+  echo ""
+}
+
+main "$@"
