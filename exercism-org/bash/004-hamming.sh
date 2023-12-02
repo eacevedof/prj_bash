b8418c2daf7fae494806431a483ae074a9db5c66
ioedumac <eacevedof@yahoo.es>
2023-12-02 14:44:14 +0100

    len!=

--- exercism-org/bash/004-hamming.sh
+++ exercism-org/bash/004-hamming.sh
@@ -20,12 +20,13 @@ main () {
     return 1
   fi
 
-  IFS=""
-  read -ra arr_sequence1 <<< "$str_sequence1"
-  read -ra arr_sequence2 <<< "$str_sequence2"
+  #printf "%s | %s" "$str_sequence1" "$str_sequence2"; return 1;
 
-  len_seq1=${#arr_sequence1[@]}
-  len_seq2=${#arr_sequence2[@]}
+
+  len_seq1=${#str_sequence1}
+  len_seq2=${#str_sequence2}
+
+  #printf "%s | %s" "$len_seq1" "$len_seq2"; return 1;
 
   if (( len_seq1 != len_seq2 ))
   then
@@ -33,25 +34,47 @@ main () {
     return 1
   fi
 
-  hamming_distance=$(get_hamming_distance arr_sequence1 arr_sequence2)
-  echo "$hamming_distance"
+  arr_sequence1=()
+  arr_sequence2=()
+
+
+
+  #error aqui no pudo partir el string en varios
+  IFS= read -r -n1 -a arr_sequence1 <<< "$str_sequence1"
+  IFS= read -r -n1 -a arr_sequence2 <<< "$str_sequence2"
+
+  printf "%s " "${arr_sequence1[@]}"; return 1;
+
+
+  printf "%s %s " "$len_seq1 $len_seq2"
+  return 1
+
+
+
+  hamming_distance=$(get_hamming_distance "${arr_sequence1[@]}" "${arr_sequence2[@]}")
+  echo "distance: $hamming_distance"
   return 0
 }
 
 
 get_hamming_distance () {
-  arr_sequence1=$1
-  arr_sequence1=$2
+  local arr_sequence1=("$1[@]")
+  local arr_sequence1=("$2[@]")
+
 
   local distance=0
-  for char1 in "${arr_sequence1[@]}"; do
-    index=$((index+1))
+  local index=0
+
+  for char1 in "${arr_sequence1[@]}"
+  do
     char2="${arr_sequence2[index]}"
     if [[ "$char1" != "$char2"  ]]
     then
       distance=$((distance+1))
     fi
+    index=$((index+1))
   done
+
   echo "$distance"
 }
 
